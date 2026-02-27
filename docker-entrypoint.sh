ca69e53e1f479d4cc6de649811a263658d3303af
Mael <maelmoreau21@gmail.com>
2026-02-27 18:46:36 +0100

    chore: Skip Prisma client generation during `db push` in the docker entrypoint script.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ echo "Starting JellyTulli Server..."
 
 # Run Prisma schema push. Note: Using `db push` to force schema creation without migrations folder.
 echo "Running Prisma db push..."
-npx prisma db push --accept-data-loss
+npx prisma db push --accept-data-loss --skip-generate
 
 echo "Prisma schema pushed successfully."
 
